232e41260f2c2984b1aa34f78e1c5a1328b03e2b
Vijay <vijaykumar@Vijays-MacBook-Air.local>
2026-05-26 11:07:26 +0530

    Use track fullscreen maximize and minimize icons

--- pwa-speed.js
+++ pwa-speed.js
@@ -1,5 +1,5 @@
 (() => {
-  const SPEED_VERSION = '20260526_track_hide_scrollbar1';
+  const SPEED_VERSION = '20260526_track_maxmin_icons1';
   const APP_PAGES = [
     'menu.html',
     'cart.html',
